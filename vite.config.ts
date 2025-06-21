b6de2578d4597b83b1c90f5e5b898082d90828d6
MetinMite <ddddmetin@gmail.com>
2025-06-21 21:27:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/metin-kilic-ozgecmis-portali/', // <-- EKLENEN SİHİRLİ SATIR BU
+  
   server: {
     host: "::",
     port: 8000,
