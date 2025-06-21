af47aed151054d25bca4018afb6c122a4870d93f
MetinMite <ddddmetin@gmail.com>
2025-06-21 21:34:09 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  
+  base: '/metin-kilic-ozgecmis-portali/',
   server: {
     host: "::",
     port: 8000,
